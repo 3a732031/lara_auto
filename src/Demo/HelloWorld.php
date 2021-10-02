582ed964a1856e707ab7e55829b1b1597c40a83a
jing yuan chen <3a732031@gm.student.ncut.edu.tw>
2021-10-02 23:34:42 +0800

    HelloWorld

--- src/Demo/HelloWorld.php
+++ src/Demo/HelloWorld.php
@@ -1,1 +1,11 @@
 <?php
+namespace Demo;/*宣告HelloWorld所在的name space為Demo*/
+
+/*HelloWorld的完整類別名稱為：\Demo\HelloWorld*/
+class HelloWorld
+{
+    function __construct()
+    {
+        echo 'Hello World~'.PHP_EOL;
+    }
+}
