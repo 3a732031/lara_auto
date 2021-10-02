6fdd21eb3e1d8a40aa11bc91bf30d2e012ee4631
jing yuan chen <3a732031@gm.student.ncut.edu.tw>
2021-10-02 23:42:45 +0800

    Lara.php

--- src/Demo/Hello/Lara.php
+++ src/Demo/Hello/Lara.php
@@ -1,1 +1,11 @@
 <?php
+namespace Demo\Hello;/* Demo\Hello為Lara所在的name space. Hello為Demo的subnamespace*/
+
+/*Lara的完整類別名稱為：\Demo\Hello\Lara*/
+class Lara
+{
+    function __construct()
+    {
+        echo 'Hello Lara'.PHP_EOL;
+    }
+}
